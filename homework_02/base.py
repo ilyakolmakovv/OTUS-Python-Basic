97d5d01db34138e5447aa156f752bc4c6a88e07f
Ilya Kolmakov <kolmakov525@gmail.com>
2023-07-24 08:25:35 +0300

    Add homework_2

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,23 @@
 from abc import ABC
+from homework_02.exceptions import LowFuelError, NotEnoughFuel
 
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight, fuel, fuel_consumption):
+        self.weight = weight
+        self.started = False
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if not self.started and self.fuel == 0:
+            raise LowFuelError
+        self.started = True
+
+    def move(self, distance):
+        full_need = distance * self.fuel_consumption
+        if full_need > self.fuel:
+            raise NotEnoughFuel
+        self.fuel -= full_need
+
+
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,24 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+
+class Plane(Vehicle):
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self.max_cargo = max_cargo
+        self.cargo = 0
+
+    def load_cargo(self, num):
+        check = self.cargo + num
+        if check > self.max_cargo:
+            raise CargoOverload
+        self.cargo += num
+
+    def remove_all_cargo(self):
+        cargo_before_null = self.cargo
+        self.cargo = 0
+        return cargo_before_null
+
